d167b37c689fc2608c84f3d7c12229d413422469
charmii03 <charmigangani46@gmail.com>
2022-07-06 14:07:07 +0530

    first commit

---
+++ makeBricks.js
@@ -0,0 +1,13 @@
+function makeBricks(small, big, goal){
+    // not enough bricks
+    if (goal > big * 5 + small) {
+      return false;
+    }
+  
+      // not enough small bricks
+    if (goal % 5 > small) {
+      return false;
+    }
+  
+    return true;
+  }
